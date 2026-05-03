5d0633a6feec713448af3142910027edf749ac75
Carlos A <cayala.w@gmail.com>
2026-05-03 12:05:28 -0500

    ruby_version: setup

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Feedback system
 
 System to gather the user's feedback and display them to an admin.
+
+Source for the ruby_version: [Honeybadger - Building a Sinatra app in Ruby](https://github.com/iamaestimo/sinatra-url-shortener-tutorial-app)
---
+++ ruby_version/config/application.rb
@@ -0,0 +1,10 @@
+require 'active_record'
+require 'sqlite3'
+require 'yaml'
+
+config_path = File.join(__dir__, 'database.yml')
+ActiveRecord::Base.configurations = YAML.load_file(config_path)
+ActiveRecord::Base.establish_connection(:development)
+
+# Loads all models
+Dir["#{__dir__}/../models/*.rb"]./each { |file| require file }
