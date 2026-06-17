b3b03469406672f36dba3c616c75f68cd6f4248e
Carlos A <cayala.w@gmail.com>
2026-06-17 00:26:31 -0500

    add helpers to config

--- ruby_version/config/application.rb
+++ ruby_version/config/application.rb
@@ -7,4 +7,7 @@ ActiveRecord::Base.configurations = YAML.load_file(config_path)
 ActiveRecord::Base.establish_connection(:development)
 
 # Loads all models
-Dir["#{__dir__}/../models/*.rb"].each { |file| require file }
+Dir["#{__dir__}/../models/*.rb"].each { |model_file| require model_file }
+
+# Loads all helpers
+Dir["#{__dir__}/../helpers/*.rb"].sort.each { |helper_file| require helper_file}
