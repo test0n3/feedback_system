ad0164680fc55e9f12e79e1848a9c3990751a303
Carlos A <cayala.w@gmail.com>
2026-08-20 23:53:16 -0500

    update app and configuration for testing

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'yaml'
 
 config_path = File.join(__dir__, 'database.yml')
 ActiveRecord::Base.configurations = YAML.load_file(config_path)
-ActiveRecord::Base.establish_connection(:development)
+ActiveRecord::Base.establish_connection(ENV.fetch('RACK_ENV', 'development').to_sym)
 
 # Loads all models
 Dir["#{__dir__}/../models/*.rb"].each { |model_file| require model_file }
